e504c42c9c7e0358d0532b67b6adca1ece0e3d66
york <787927530@qq.com>
2019-04-15 13:49:23 +0800

    push

--- app/src/main/java/com/android/tool/util/LoadingDialogUtil.java
+++ app/src/main/java/com/android/tool/util/LoadingDialogUtil.java
@@ -41,6 +41,10 @@ public class LoadingDialogUtil {
         LinearLayout layout = (LinearLayout) v.findViewById(R.id.dialog_loading_view);// 加载布局
         TextView tipTextView = (TextView) v.findViewById(R.id.tipTextView);// 提示文字
         tipTextView.setText(msg);// 设置加载信息
+        SpinKitView spinKit = (SpinKitView) v.findViewById(R.id.spin_kit);
+        spinKit.setIndeterminateDrawable( new Circle());
+//        spinKit.setIndeterminateDrawable( new FadingCircle());
+//        spinKit.setIndeterminateDrawable( new ThreeBounce());
         Dialog loadingDialog = new Dialog(context, R.style.loadingStyle);// 创建自定义样式dialog
         loadingDialog.setCancelable(true); // 是否可以按“返回键”消失
         loadingDialog.setCanceledOnTouchOutside(false); // 点击加载框以外的区域
